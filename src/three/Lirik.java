578fb376aa304048423141dd1e69bd1ff51a0b94
wignn <tigfiragnafatur1933@gmail.com>
2024-08-31 22:40:57 +0700

    first commit

---
+++ src/three/Lirik.java
@@ -0,0 +1,7 @@
+package three;
+
+public class Lirik {
+    public Lirik (String params){
+            System.out.println(params);
+    }
+}
---
+++ src/two/InterfaceMatematika.java
@@ -0,0 +1,8 @@
+package two;
+
+public interface InterfaceMatematika {
+    void pertambahan(int a, int b);
+    void pengurangan(int a, int b);
+    void perkalian(int a, int b);
+    void pembagian(int a, int b);
+}
